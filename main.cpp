d044fc96dd8c4b11033d3a7d61a460ccdc87f83d
Benjamin Worthington <benworthin@icloud.com>
2021-02-18 13:01:14 -0600

    Completed step 1 and 2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ int main()
 
     // Test the student list class
     StudentList list;
-    list.insertHead("John", "Johnson, AR", 3.2);
+
+//    list.insertHead("Ben W", "Fayetteville, AR", 3.4);
+//    list.insertHead("Jackson D", "Fayetteville, AR", 3.8);
+//    list.insertHead("Andy W", "Bella Vista, AR", 4.5);
+//    list.insertHead("Josh G", "Fayetteville, AR", 3.9);
+//    list.insertHead("Joel P", "Fayetteville, AR", 4.0);
+
+    list.insertTail("Ben W", "Fayetteville, AR", 3.4);
+    list.insertTail("Jackson D", "Fayetteville, AR", 3.8);
+    list.insertTail("Andy W", "Bella Vista, AR", 4.5);
+    list.insertTail("Josh G", "Fayetteville, AR", 3.9);
+    list.insertTail("Joel P", "Fayetteville, AR", 4.0);
+
     list.print();
-    
+
 }
